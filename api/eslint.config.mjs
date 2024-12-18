5fee0b17f05d55af3a373adf1415d6add8e60140
Victor Valenzuela <khancerberus@hotmail.cl>
2024-12-18 11:10:39 -0300

    chore: Disable 'no-explicit-any' rule in ESLint configuration for TypeScript

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -6,4 +6,11 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
+  {
+    rules: {
+      'no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+
+  }
 );
