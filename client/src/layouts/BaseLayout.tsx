10d53bfe6f91f970c793359aba37c6a2794ba8b4
Victor Valenzuela <khancerberus@hotmail.cl>
2024-12-20 02:30:03 -0300

    feat: add Toaster component for enhanced notification handling in BaseLayout

--- client/src/layouts/BaseLayout.tsx
+++ client/src/layouts/BaseLayout.tsx
@@ -2,6 +2,7 @@ import { PrimeReactProvider } from 'primereact/api';
 import { Outlet } from 'react-router-dom';
 import { Navigator } from '@/components/Navigator';
 import Tailwind from 'primereact/passthrough/tailwind';
+import { Toaster } from "sonner"
 
 export const BaseLayout = () => {
   return (
@@ -11,6 +12,7 @@ export const BaseLayout = () => {
         pt: Tailwind,
       }}
     >
+      <Toaster theme="dark" richColors />
       <main className="flex flex-col gap-5 w-full justify-center items-center">
         <Navigator />
         <Outlet />
